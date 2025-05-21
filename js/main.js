536290f22b101ab3d6d6db3d252ba0aa4adc76cc
K.M. AL-AMIN <alaminkhanstd123@gmail.com>
2025-05-21 17:02:56 +0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,46 +1,14 @@
-  const firstColor = document.getElementById("firstColor");
-  const secondColor = document.getElementById("secondColor");
-  const angle = document.getElementById("radientAngle");
-  const textColor = document.getElementById("textColor");
-  const opacity = document.getElementById("opacity");
+// footer pop / hover elm close and hover functionality
 
-  const outFirst = document.getElementById("outFirst");
-  const outSecond = document.getElementById("outSecond");
-  const outAngle = document.getElementById("outAngle");
-  const angleVal = document.getElementById("angleVal");
-  const opacityVal = document.getElementById("opacityVal");
+const officeClickElm = document.querySelectorAll('.office-hov-cls')
+const officeAddrElm = document.querySelectorAll('.office-address')
+const mainOfficeAddrElm = document.querySelectorAll('.office-details')
 
-  const preview = document.getElementById("outputPreview");
-
-  function updateGradient() {
-    // Update the preview background and text color
-    const angleDeg = angle.value;
-    const color1 = firstColor.value;
-    const color2 = secondColor.value;
-    const txtColor = textColor.value;
-    const opac = opacity.value;
-
-    // Set background gradient
-    preview.style.background = `linear-gradient(${angleDeg}deg, ${color1}, ${color2})`;
-
-    // Set text color and opacity
-    preview.style.color = txtColor;
-    // preview.style.opacity = opac;
-
-    // Show live output values
-    outFirst.textContent = color1;
-    outSecond.textContent = color2;
-    outAngle.textContent = angleDeg;
-    angleVal.textContent = angleDeg;
-    // opacityVal.textContent = opac;
-  }
-
-  // Add event listeners
-  firstColor.addEventListener("input", updateGradient);
-  secondColor.addEventListener("input", updateGradient);
-  angle.addEventListener("input", updateGradient);
-  textColor.addEventListener("input", updateGradient);
-  opacity.addEventListener("input", updateGradient);
-
-  // Run once on load
-  updateGradient();
+for(let i = 0; i < officeClickElm.length; i++){
+  officeClickElm[i].addEventListener('click', ()=> {
+    officeAddrElm[i].style.visibility = 'hidden'
+  })
+  mainOfficeAddrElm[i].addEventListener('mouseover', ()=> {
+    officeAddrElm[i].style.visibility = 'visible'
+  })
+}
